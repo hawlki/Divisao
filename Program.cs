d4df00dabcf3111b6f43477e4abffb48422762cc
israel lopes <israel.lopes2710@gmail.com>
2021-04-20 12:04:26 -0300

    finaliza o programa

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace divisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.Beep();
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("          Divisão          ");
+            Console.WriteLine("---------------------------");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("Insira aqui um número para ser o numerador: ");
+            bool NValido = Decimal.TryParse(Console.ReadLine(), out decimal numerador);
+            Console.Write("Insira aqui um número para ser o denominador: ");
+            bool DValido = Decimal.TryParse(Console.ReadLine(), out decimal denominador);
+            Console.WriteLine();
+
+            if(!NValido || !DValido)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Digite somente números");
+                Console.ResetColor();
+                Environment.Exit(1);
+            }
+
+            if(denominador == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("O denominador digitado não pode se usado para dividir, por favor insira um número que não seja zero");
+                Console.ResetColor();
+                Environment.Exit(1);
+            }
+
+            else
+            {
+                decimal resultado = numerador / denominador;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine($"{numerador} dividido por {denominador} é {resultado}");
+
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Toque em uma tecla para finalizar o programa");
+                Console.ReadKey();
+
+                Console.Clear();
+            }
+
+        }
+    }
+}
